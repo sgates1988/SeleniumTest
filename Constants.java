ed72bad8f4b6c71a3375d1bd3a5ff0f9545f35e3
Sharnita Gates <sgates1988@gmail.com>
2017-04-18 11:33:56 -0400

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,8 @@
+  Abstract Class Constants {
+        private static multilineUsernm = "2145359548";
+        private static multilinePwd = "Unix1234";
+        private static multilineUsernmTwo = "2145359503";
+       
+        private static singlelineUsernm = "2145359548";
+        private static singlelinePwd = "Unix1234";
+}
