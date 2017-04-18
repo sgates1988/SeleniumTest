20fcd97d53d614ec51cc9bb9b8538303bc133694
Sharnita Gates <sgates1988@gmail.com>
2017-04-18 11:31:45 -0400

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -4,7 +4,7 @@ public Class HomePage extends Page {
         static String acctSummTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[1]/a"));
         static String paymentsTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[2]"));
         static String billHistTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[3]/a"));
-        static String usageTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[4]""));
+        static String usageTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[4]"));
         static String lineDtlsTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[5]/a"));
         static String acctSettTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[6]"));
 
