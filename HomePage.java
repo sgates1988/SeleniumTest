27e8dc36ece1f3dd2e6eeea3d980ec38f30b62ab
Sharnita Gates <sgates1988@gmail.com>
2017-04-18 11:31:12 -0400

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -7,4 +7,14 @@ public Class HomePage extends Page {
         static String usageTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[4]""));
         static String lineDtlsTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[5]/a"));
         static String acctSettTab = driver.findElement(By.xpath("//*[@id="myAccountMenuSub"]/li[6]"));
+
+    //Add Funds Button
+    static String addFundsBtn = driver.findElement(By.xpath("//*[@id="container"]/section/div/overview/div[2]/div[1]/div/div/myc-panel/div/div[2]/myc-panel-body/div[1]/div/div/div/div[2]/button"));
+
+    //Billing
+    static String monthTotl = driver.findElement(By.xpath("//*[@id="container"]/section/div/overview/div[2]/div[1]/div/div/myc-panel/div/div[2]/myc-panel-body/div[1]/div/div/div/div[1]/div[1]/div/div[2]/span"));
+    static String acctCdt= driver.findElement(By.xpth("//*[@id="container"]/section/div/overview/div[2]/div[1]/div/div/myc-panel/div/div[2]/myc-panel-body/div[1]/div/div/div/div[1]/div[2]/div/div[2]/span”));
+    static String amtDue= driver.findElement(By.cssSelector("input[ng-class="bill-due-line-item”]”));
+    static String dueDate = driver.findElement(By.cssSelector("input[ng-class="bill-due-line-item”]”));
+
 }
