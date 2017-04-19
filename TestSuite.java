f86eaf2d22607e2f63e17773bd84fcfc523b0263
Sharnita Gates <sgates1988@gmail.com>
2017-04-19 10:52:46 -0400

    Update TestSuite.java

--- TestSuite.java
+++ TestSuite.java
@@ -4,17 +4,13 @@ public Class TestSuite {
 public Class tc_0826_myaccount_multiline_line_details_view() {
     
 //Given: I am logged into a multiline accout
-LoginFlows.singlelineUser_Login();
-
+LoginFlows.multilineUser_Login();
 
 //When: I select line details from the menu
-
 HomePageFlows.getLineDetailsTab();
 
 //Then: I land on the line details view with "your lines" heading expanded 
-
-
-
+LineDetlsFlow.verifyYourLineHeaderPresentAndExpanded();
 
 }
 // Test case for singleline line details 
@@ -22,16 +18,20 @@ HomePageFlows.getLineDetailsTab();
 public Class tc_0827_myaccount_singleline_line_details_view() {
     
 //Given: I am logged into a singleline accout
-
-
+LoginFlows.singlelineUser_Login();
+	
 //When: I select line details from the menu
-
+HomePageFlows.getLineDetailsTab();
 
 //Then: I land on the line details view for that line
 
+LineDtlsFlows.verifyPhoneNumberHeader(String, Null);
+		
 //And:the "your lines" heading is not present in the view
+LineDtlsFlows.verifyYourLineHeaderNotPresent();
 
 //And: the usage, plan, features, device and cricket rewards headings are available and all expanded 
+LineDtlsFlows.verifyAllMenuHeadersPresentAndExpanded();
 	}	 
-	
+
 }
