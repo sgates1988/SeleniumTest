912b87d3aaea7332e13011fab2a51736d60a4ed3
Sharnita Gates <sgates1988@gmail.com>
2017-04-19 09:41:34 -0400

    Update LineDetailsFlows.java

--- LineDetailsFlows.java
+++ LineDetailsFlows.java
@@ -1,15 +1,17 @@
+import static org.junit.Assert.assertTrue;
+import org.openga.selenium.WebDriver;
+
 public Class LineDtlsFlows {
 
-LoginPage page = new LoginPage();
-
-WebDriver driver = new WebDriver();
-WebDriverWait wait = new WebDriverWait(10);
+	LoginPage page = new LoginPage();
+	WebDriver driver = new WebDriver();
+	WebDriverWait wait = new WebDriverWait(10, SECONDS);
 
 //Checks for Multiline Users
 public static  void verifyMultiLineUsersPresent(String value) {
     driver.wait.until(ExpectedConditions.elementToBeClickable(lineOne)));
 
-String lineOneTest = lineOne.getText();
+	String lineOneTest = lineOne.getText();
     if (assertTrue(lineOneTest.contains(multilineUsernm))) {;
             System.out.println("Line One User is Valid");
         } else {
@@ -33,55 +35,62 @@ public static  void verifySingleLineUserPresent() {
             System.out.println("User is invalid for Line One")
         }
     }
-}
-    // Verify "Your Lines" header is present and expanded
-    public static void verifyYourLineHeaderIsPresentAndExpanded() {
-        driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
-        if (lnUsageExClpsBtn = 'true') {
-            System.out.println("Usage section is expanded");
-            } else {
-                System.out.println("Usage section is no expanded")
-            }
-             driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
-        if (planExClpsBtn = 'true') {
-            System.out.println("Plan section is expanded");
-            } else {
-                System.out.println("Plan section is no expanded")
-            }
-             driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
-        if (featureExClpsBtn = 'true') {
-            System.out.println("Features section is expanded");
-            } else {
-                System.out.println("Features section is no expanded")
-            }
-             driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
-        if (deviceExClpsBtn = 'true') {
-            System.out.println("Device section is expanded");
-            } else {
-                System.out.println("Device section is no expanded")
-            }
-             driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
-        if (crktRwdsExClpsBtn = 'true') {
-            System.out.println("Cricket Rewards section is expanded");
-            } else {
-                System.out.println("Cricket Rewards section is no expanded")
-            }
-    }
-    // Verify "Your Lines" header is NOT present
-    public static void verifyYourLineHeaderIsNotPresent() {
-       
-    }    
     
-}
-
+    // Verify "Your Lines" header is present and expanded
+    public static void verifyYourLineHeaderPresentAndExpanded() {
+       String outcome = "true"; 
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(lineUsageHeader));
+        assertTrue("Verification Passed: Your Lines section is found.",(lineUsageHeader).equalsIgnoreCase(outcome)));
+    
+    // Verify "Your Lines" header is NOT present
+        
+    public static void verifyYourLineHeaderNotPresent() {
+       String outcome = "false"; 
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(lineUsageHeader));
+        assertTrue("Verification Passed: Your Lines section is NOT found.",(lineUsageHeader).equalsIgnoreCase(outcome)));
+         
+    }       
+    
 //Verify Menu headers present Usage, Plan, Features, Device, and Cricket Rewards is present and expanded
-
-    public static void verifyAllMenuHeadersIsPresentAndExpanded() {
-       
+    public static void verifyAllMenuHeadersPresentAndExpanded() {
+       String outcome = "true"; 
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
+        assertTrue("Verification Passed: Usage section is expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
+              
+        driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
+        assertTrue("Verification Passed: Plan section is expanded.",(planExpdSectn).equalsIgnoreCase(outcome)));
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(featureExpdSectn));
+        assertTrue("Verification Passed: Feature section is expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(deviceExpdSectn));
+        assertTrue("Verification Passed: Device section is expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(crktRwdsExpdSectn));
+        assertTrue("Verification Passed: Rewards section is expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
     }
+    
     
 //Verify Menu headers present Usage, Plan, Features, Device, and Cricket Rewards is NOT present 
- public static void verifyAllMenuHeadersIsNotPresent() {
-       
+ public static void verifyAllMenuHeadersNotPresent() {
+       String outcome = "false"; 
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
+        assertTrue("Verification Passed: Usage section is NOT expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
+              
+        driver.wait.until(ExpectedConditions.elementToBeClickable(lnUsageExClpsBtn));
+        assertTrue("Verification Passed: Plan section is NOT expanded.",(planExpdSectn).equalsIgnoreCase(outcome)));
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(featureExpdSectn));
+        assertTrue("Verification Passed: Feature section is NOT expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(deviceExpdSectn));
+        assertTrue("Verification Passed: Device section is NOT expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
+        
+        driver.wait.until(ExpectedConditions.elementToBeClickable(crktRwdsExpdSectn));
+        assertTrue("Verification Passed: Rewards section is NOT expanded.",(lnUsageExpdSectn).equalsIgnoreCase(outcome)));
     }
 }
